ca06deb8c66f458490c70ac9adb24e27e2bb5328
leppaino <43615150+leppaino@users.noreply.github.com>
2018-12-07 20:23:53 +0200

    Add files via upload

--- koodi/viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ koodi/viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -4,9 +4,11 @@ import statistics.matcher.*;
 
 public class Main {
     public static void main(String[] args) {
+        Matcher m;
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
           
-        Matcher m = new And( new HasAtLeast(10, "goals"),
+        System.out.println(" And " );
+        m = new And( new HasAtLeast(5, "goals"),
                              new HasAtLeast(10, "assists"),
                              new PlaysIn("PHI")
         );
@@ -14,5 +16,33 @@ public class Main {
         for (Player player : stats.matches(m)) {
             System.out.println( player );
         }
+        
+        System.out.println("\n\n Not " );
+        
+        m = new Not( new HasAtLeast(1, "goals") );
+
+        for (Player player : stats.matches(m)) {
+            System.out.println( player );
+        }
+
+        System.out.println("\n\n Or" );
+
+        m = new Or( new HasAtLeast(40, "goals"),
+                    new HasAtLeast(60, "assists"),
+                    new HasAtLeast(85, "points")
+        );   
+        for (Player player : stats.matches(m)) {
+            System.out.println( player );
+        }
+        
+        System.out.println("\n\n HasFewerThan" );
+
+        //m = new Not( new HasFewerThan(1, "goals"));
+        m = (Matcher) new HasFewerThan(1, "goals");  
+        for (Player player : stats.matches(m)) {
+            System.out.println( player );
+        }
+        
+        
     }
 }
